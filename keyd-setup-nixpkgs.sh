2ea3c2e50d6de53e4620606e927e73511d37b954
amjadodeh <69196058+amjadodeh@users.noreply.github.com>
2023-09-21 01:26:36 -0400

    added script that uses nixpkgs to install keyd (not tested yet)

---
+++ keyd-setup-nixpkgs.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+sh <(curl -L https://nixos.org/nix/install) --daemon
+
+nix-env -iA nixpkgs.keyd
+
+systemctl enable --now keyd.service
+
+sudo mkdir -p /etc/keyd/
+
+ln -s /usr/share/keyd/keyd.compose ~/.XCompose
+
+echo "[ids]
+
+*
+
+[main]
+
+leftalt = layer(alt)
+
+[alt]
+
+a = ā
+d = ḍ
+h = ḥ
+i = ī
+s = ṣ
+t = ṭ
+u = ū
+z = ẓ
+l = ʿ
+j = ʾ
+esc = ~
+
+[alt+shift]
+
+a = Ā
+d = Ḍ
+h = Ḥ
+i = Ī
+s = Ṣ
+t = Ṭ
+u = Ū
+z = Ẓ
+l = ʿ
+j = ʾ
+" | sudo tee /etc/keyd/default.conf
+
+systemctl restart keyd.service
+
+echo "Done!"
+
