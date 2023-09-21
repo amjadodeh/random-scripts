971fc2ad6ea77f5f5ea729887f196c1096d5bc65
amjadodeh <69196058+amjadodeh@users.noreply.github.com>
2023-09-20 23:45:02 -0400

    fixed unicode stuff

--- keyd-setup-opensuse.sh
+++ keyd-setup-opensuse.sh
@@ -17,29 +17,29 @@ leftalt = layer(alt)
 [alt]
 
 a = macro(C-S-u 0101 space)
-d = macro(C-S-u 1E0D space)
-h = macro(C-S-u 1E25 space)
-i = macro(C-S-u 012B space)
-s = macro(C-S-u 1E63 space)
-t = macro(C-S-u 1E6D space)
-u = macro(C-S-u 016B space)
-z = macro(C-S-u 1E93 space)
-l = macro(C-S-u 02BF space)
-j = macro(C-S-u 02BE space)
+d = macro(C-S-u 1e0d space)
+h = macro(C-S-u 1d25 space)
+i = macro(C-S-u 012b space)
+s = macro(C-S-u 1e63 space)
+t = macro(C-S-u 1e6d space)
+u = macro(C-S-u 016b space)
+z = macro(C-S-u 1e93 space)
+l = macro(C-S-u 02bf space)
+j = macro(C-S-u 02be space)
 esc = ~
 
 [alt+shift]
 
 a = macro(C-S-u 0100 space)
-d = macro(C-S-u 1E0C space)
-h = macro(C-S-u 1E24 space)
-i = macro(C-S-u 012A space)
-s = macro(C-S-u 1E62 space)
-t = macro(C-S-u 1E6C space)
-u = macro(C-S-u 016A space)
-z = macro(C-S-u 1E92 space)
-l = macro(C-S-u 02BF space)
-j = macro(C-S-u 02BE space)
+d = macro(C-S-u 1e0c space)
+h = macro(C-S-u 1e24 space)
+i = macro(C-S-u 012a space)
+s = macro(C-S-u 1e62 space)
+t = macro(C-S-u 1e6C space)
+u = macro(C-S-u 016a space)
+z = macro(C-S-u 1e92 space)
+l = macro(C-S-u 02bf space)
+j = macro(C-S-u 02be space)
 " | sudo tee /etc/keyd/default.conf
 
 systemctl restart keyd.service
