fef613fde07b54cf8c3f838281c4e9580577ff86
amjadodeh <69196058+amjadodeh@users.noreply.github.com>
2023-10-10 08:56:02 -0400

    added keyd-setup-alpine.sh

---
+++ keyd-setup-alpine.sh
@@ -0,0 +1,57 @@
+#!/bin/ash
+
+echo "
+https://dl-cdn.alpinelinux.org/alpine/v$(cut -d'.' -f1,2 /etc/alpine-release)/main/
+https://dl-cdn.alpinelinux.org/alpine/v$(cut -d'.' -f1,2 /etc/alpine-release)/community/
+https://dl-cdn.alpinelinux.org/alpine/edge/testing/
+" | doas tee /etc/apk/repositories
+
+doas apk update
+
+doas apk add keyd
+
+doas mkdir -p /etc/keyd/
+
+ln -s /usr/share/keyd/keyd.compose ~/.XCompose
+
+echo "[ids]
+
+*
+
+[main]
+
+leftalt = layer(alt)
+
+[alt]
+
+a = ā
+d = ḍ
+h = ḥ
+i = ī
+s = ṣ
+t = ṭ
+u = ū
+z = ẓ
+l = ʿ
+j = ʾ
+esc = ~
+
+[alt+shift]
+
+a = Ā
+d = Ḍ
+h = Ḥ
+i = Ī
+s = Ṣ
+t = Ṭ
+u = Ū
+z = Ẓ
+l = ʿ
+j = ʾ
+" | doas tee /etc/keyd/default.conf
+
+doas rc-update add keyd default
+doas rc-service keyd start
+
+echo "Done! Please restart your applications for this to take effect."
+
